0488de1ac06269598f4906950000b8ea4bbeb31a
HaoxuanXu347 <hxu347@wisc.edu>
2023-07-21 22:24:46 +0800

    7.21 Version2

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from flask import Flask, request, render_template
 import datetime
 import re
 import time
-from cachetools import TTLCache
-from tenacity import retry, stop_after_attempt, wait_exponential
 
 app = Flask(__name__)
 
@@ -18,13 +16,13 @@ def extract_keywords(url):
     r = Rake(language='english',
              ranking_metric=Metric.WORD_DEGREE, 
              include_repeated_phrases=False,
-             min_length=1, max_length=2)
+             min_length=1, max_length=3)
 
     r.extract_keywords_from_text(soup.body.get_text(' ', strip=True))
 
     # Extract at maximum of 3 keywords
     for rating, keyword in r.get_ranked_phrases_with_scores():
-        if len(Keywords) == 3:
+        if len(Keywords) == 6:
             break
         if rating > 8:
             Keywords.append(keyword) 
@@ -33,15 +31,7 @@ def extract_keywords(url):
 
     return Keywords
 
-# Initialize a cache with a time-to-live (TTL) of 60 seconds
-cache = TTLCache(maxsize=100, ttl=60)
-
-@retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=2, max=10))
 def fetch_google_trends_data(keyword):
-    # Check if the data is in the cache
-    if keyword in cache:
-        return cache[keyword]
-
     # Create a pytrends object
     pytrends = TrendReq(hl='en-US', tz=360)
 
@@ -52,55 +42,41 @@ def fetch_google_trends_data(keyword):
     # Get the interest over time data as a Pandas DataFrame
     trends_data = pytrends.interest_over_time()
 
-    # Store the data in the cache
-    cache[keyword] = trends_data
+    time.sleep(2)
+
+    # Get the related topics for the provided keyword
+    related_topics_data = pytrends.related_topics()
 
     # Add a delay of 2 seconds between requests
     time.sleep(2)
 
-    return trends_data
-
-def get_related_images(keyword):
-    # Create a pytrends object
-    pytrends = TrendReq(hl='en-US', tz=360)
-
-    # Get related queries for the keyword
-    pytrends.build_payload([keyword], cat=0, timeframe='today 1-m', geo='US', gprop='')
-    related_queries = pytrends.related_queries()
-
-    # Get the related image for the keyword
-    related_queries_top = related_queries[keyword]['top']
-    related_image = related_queries_top['image_url'] if related_queries_top is not None else None
-
-    return related_image
+    return trends_data, related_topics_data
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     keywords = []
     trends_data = None
-    related_image = None
+    related_topics_data = None
     message = None
 
     if request.method == 'POST':
         url = request.form.get('url')
-        keywords = extract_keywords(url)
-
-        # Debugging statement: Print the extracted keywords
-        print("Extracted Keywords:", keywords)
+        keywords = extract_keywords(url) 
 
         if not keywords:
             message = "No relevant keywords found in the given URL. Please try again with a different URL."
         else:
             try:
-                trends_data = fetch_google_trends_data(keywords[0])
-                # Debugging statement: Print the trends_data
+                trends_data, related_topics_data = fetch_google_trends_data(keywords[0])
+                # Debugging statements: Print the trends_data and related_topics_data
                 print("Google Trends Data:", trends_data)
+                print("Related Topics Data:", related_topics_data)
 
                 if trends_data is None or trends_data.empty:
                     message = "No Google Trends data available for the entered keyword."
-                else:
-                    related_image = get_related_images(keywords[0])
             except Exception as e:
                 message = "Error fetching Google Trends data. Please try again later."
 
-    return render_template('home.html', keywords=keywords, trends_data=trends_data, related_image=related_image, message=message)
+    return render_template('home.html', keywords=keywords, trends_data=trends_data, related_topics_data=related_topics_data, message=message)
+
+
